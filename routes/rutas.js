ffb0932d7263a8fde52c1b935bb0901b46d40921
OneyverRodriguezN <oneyver.rodriguez@unipamplona.edu.co>
2024-04-05 00:09:19 -0500

    Subida Archivos

---
+++ routes/rutas.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const router = express.Router();
+const clienteController = require('../controllers/clienteController')
+
+// Rutas de CRUD
+
+router.post('/',clienteController.agregarClientes);
+router.get('/',clienteController.buscarClientes);
+router.get('/:id',clienteController.buscarCliente);
+router.delete('/:id',clienteController.eliminarCliente);
+router.put('/:id',clienteController.actualizarCliente);
+
+module.exports = router;
